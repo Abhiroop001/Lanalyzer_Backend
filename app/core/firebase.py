69221fae236097be52bdd10378c8ee0774e9983c
Abhiroop001 <Chandanamukherjee9804@gmail.com>
2026-03-22 16:10:10 +0530

    Update firebase.py

--- app/core/firebase.py
+++ app/core/firebase.py
@@ -1,8 +1,17 @@
+import os, json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-if not firebase_admin._apps:
-    cred = credentials.Certificate("firebase_key.json")
-    firebase_admin.initialize_app(cred)
+# Load JSON string from environment variable
+firebase_credentials_json = os.getenv("FIREBASE_CREDENTIALS_JSON")
 
-db = firestore.client()
+if firebase_credentials_json:
+    creds_dict = json.loads(firebase_credentials_json)
+    cred = credentials.Certificate(creds_dict)
+
+    if not firebase_admin._apps:
+        firebase_admin.initialize_app(cred)
+
+    db = firestore.client()
+else:
+    raise RuntimeError("FIREBASE_CREDENTIALS_JSON not set in environment")
